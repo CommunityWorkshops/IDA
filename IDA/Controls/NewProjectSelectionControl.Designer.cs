ed1d295df142e7750813c1becfe0621b838da387
7ASecond-Net <admin@7asecond.net>
2016-07-04 10:37:54 +0100

    Added New Project Selection Control

---
+++ IDA/Controls/NewProjectSelectionControl.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace IDA.Controls
+{
+    partial class NewProjectSelectionControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pbSelectionImage = new System.Windows.Forms.PictureBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSelectionImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.lblName);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 117);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(150, 33);
+            this.panel1.TabIndex = 0;
+            // 
+            // pbSelectionImage
+            // 
+            this.pbSelectionImage.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbSelectionImage.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pbSelectionImage.Location = new System.Drawing.Point(12, 4);
+            this.pbSelectionImage.Name = "pbSelectionImage";
+            this.pbSelectionImage.Size = new System.Drawing.Size(126, 108);
+            this.pbSelectionImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbSelectionImage.TabIndex = 1;
+            this.pbSelectionImage.TabStop = false;
+            this.pbSelectionImage.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pbSelectionImage_MouseClick);
+            this.pbSelectionImage.MouseEnter += new System.EventHandler(this.pbSelectionImage_MouseEnter);
+            this.pbSelectionImage.MouseLeave += new System.EventHandler(this.pbSelectionImage_MouseLeave);
+            // 
+            // lblName
+            // 
+            this.lblName.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(0, 0);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(150, 33);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Unknown";
+            this.lblName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblName.MouseClick += new System.Windows.Forms.MouseEventHandler(this.lblName_MouseClick);
+            this.lblName.MouseEnter += new System.EventHandler(this.lblName_MouseEnter);
+            this.lblName.MouseLeave += new System.EventHandler(this.lblName_MouseLeave);
+            // 
+            // NewProjectSelectionControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.pbSelectionImage);
+            this.Controls.Add(this.panel1);
+            this.Name = "NewProjectSelectionControl";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.NewProjectSelectionControl_MouseClick);
+            this.MouseEnter += new System.EventHandler(this.NewProjectSelectionControl_MouseEnter);
+            this.MouseLeave += new System.EventHandler(this.NewProjectSelectionControl_MouseLeave);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbSelectionImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.PictureBox pbSelectionImage;
+    }
+}
---
+++ IDA/Controls/NewProjectSelectionControl.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IDA.Controls
+{
+    public partial class NewProjectSelectionControl : UserControl
+    {
+
+        public delegate void ThisIsSelectedHandler();
+        public event ThisIsSelectedHandler ThisIsSelected;
+
+        private Color bgColour; 
+        public NewProjectSelectionControl()
+        {
+            InitializeComponent();
+            bgColour = lblName.BackColor;
+        }
+
+        private void NewProjectSelectionControl_MouseEnter(object sender, EventArgs e)
+        {
+            mEnter();
+        }
+        
+        private void NewProjectSelectionControl_MouseLeave(object sender, EventArgs e)
+        {
+            mLeave();
+        }
+        
+        private void NewProjectSelectionControl_MouseClick(object sender, MouseEventArgs e)
+        {
+            mClick();
+        }
+        
+        private void pbSelectionImage_MouseClick(object sender, MouseEventArgs e)
+        {
+            mClick();
+        }
+
+        private void pbSelectionImage_MouseEnter(object sender, EventArgs e)
+        {
+            mEnter();
+        }
+
+        private void pbSelectionImage_MouseLeave(object sender, EventArgs e)
+        {
+            mLeave();
+        }
+
+        private void lblName_MouseClick(object sender, MouseEventArgs e)
+        {
+            mClick();
+        }
+
+        private void lblName_MouseEnter(object sender, EventArgs e)
+        {
+            mEnter();
+        }
+
+        private void lblName_MouseLeave(object sender, EventArgs e)
+        {
+            mLeave();
+        }
+
+        private void mEnter()
+        {
+           BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+        }
+
+        private void mLeave()
+        {
+            BorderStyle = System.Windows.Forms.BorderStyle.None;
+        }
+
+       
+        private void mClick()
+        {
+            lblName.BackColor = Color.BurlyWood;
+        }
+
+        private bool _IsSelected = false;
+        public void Selected(bool isSelected)
+        {
+            if (!isSelected) lblName.BackColor = bgColour;
+            OnThisIsSelected();
+        }
+
+        protected virtual void OnThisIsSelected()
+        {
+            ThisIsSelected?.Invoke();
+        }
+    }
+}
