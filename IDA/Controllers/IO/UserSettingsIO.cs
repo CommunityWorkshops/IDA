01eb7896c2e7c0190856485716917b4a2d012d02
7ASecond-Net <admin@7asecond.net>
2016-07-05 11:34:14 +0100

    Added LoadUser

---
+++ IDA/Controllers/IO/UserSettingsIO.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IDA.Models;
+
+namespace IDA.Controllers.IO
+{
+    public class UserSettingsIo
+    {
+
+        public static void LoadUser()
+        {
+            FileStream fs = null;
+            StreamReader sr = null;
+
+            try
+            {
+                fs = new FileStream(Path.Combine("Settings","User.dat"),FileMode.Open,FileAccess.Read,FileShare.Read);
+                sr = new StreamReader(fs);
+
+                UserModel.UserName = sr.ReadLine();
+                UserModel.UserEmail = sr.ReadLine();
+
+
+                sr?.Close();
+                fs?.Close();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                sr?.Close();
+                fs?.Close();
+            }
+        }
+
+    }
+}
