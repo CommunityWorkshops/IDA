d154eb42ec89d91c78181310385215ff7b158694
7ASecond-Net <admin@7asecond.net>
2016-07-05 11:20:09 +0100

    Added User Editor

---
+++ IDA/Forms/Dialog/FrmUserEditor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
+
+namespace IDA.Forms.Dialog
+{
+    public partial class FrmUserEditor : DockContent
+    {
+        public FrmUserEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tbName.Text)) return;
+            if (string.IsNullOrEmpty(tbEmail.Text)) return;
+
+            FileStream fs = null;           
+            StreamWriter sw = null;
+
+            try
+            {
+                fs = new FileStream(Path.Combine("Settings", "User.dat"), FileMode.Create, FileAccess.Write, FileShare.None);
+                sw = new StreamWriter(fs);
+
+                sw.WriteLine(tbName.Text);
+                sw.WriteLine(tbEmail.Text);
+
+                sw?.Close();
+                fs?.Close();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                sw?.Close();
+                fs?.Close();
+            }
+           
+
+
+        }
+    }
+}
