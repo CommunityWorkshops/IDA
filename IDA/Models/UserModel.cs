e71132c060ee5dd7513e8d7d185c612c0f271a81
7ASecond-Net <admin@7asecond.net>
2016-07-05 11:24:55 +0100

    Added User Model

---
+++ IDA/Models/UserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDA.Models
+{
+    public static class UserModel
+    {
+
+        public static string UserName { get; set; }
+        public static string UserEmail { get; set; }
+
+    }
+}
