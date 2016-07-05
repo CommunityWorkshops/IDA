9c8899c4678d495e0d103b16129399ad69770704
7ASecond-Net <admin@7asecond.net>
2016-07-05 16:11:54 +0100

    Added LoadLicenceHeader

---
+++ IDA/Controllers/IO/LoadLicenceHeader.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDA.Controllers.IO
+{
+    public class LoadLicenceHeader
+    {
+
+        public static string[] Load()
+        {
+            string[] Lines;
+
+            // If default license is not available just use the licenceHeader template and make it the default
+            // If no licence header is available download it from the Community-Workshop.com website
+
+            if (!Directory.Exists("Settings")) Directory.CreateDirectory("Settings");
+            if (File.Exists(Path.Combine("Settings", "DefaultHeader.tpl"))) return LoadDefaultHeaderLicence();
+            else if (File.Exists(Path.Combine("Settings", "LicenceHeader.tpl"))) return LoadLicenceTemplate();
+            else return DownloadLicenceTemplate();
+            
+        }
+
+        private static string[] DownloadLicenceTemplate()
+        {
+            return new string[] {};
+        }
+
+        private static string[] LoadLicenceTemplate()
+        {
+            return new string[] {};
+        }
+
+        private static string[] LoadDefaultHeaderLicence()
+        {
+            return new string[] {};
+        }
+    }
+}
