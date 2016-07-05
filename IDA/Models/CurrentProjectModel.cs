de2dd434f9bc1cdebfd565705b6879a0ffba78ea
7ASecond-Net <admin@7asecond.net>
2016-07-05 15:46:33 +0100

    Added Properties

--- IDA/Models/CurrentProjectModel.cs
+++ IDA/Models/CurrentProjectModel.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace IDA.Models
 {
-    class CurrentProjectModel
+    internal static class CurrentProjectModel
     {
+
+        public static string Platform { get; set; }
+        public static string Version { get; set; }
+        public static string Name { get; set; }
+        public static bool IsLibrary { get; set; }
+        public static bool IsOpenSource { get; set; }
+        public static int MajorVersion { get; set; }
+        public static int MinorVersion { get; set; }
+        public static int MajorBuild { get; set; }
+        public static int MinorBuild { get; set; }
+
     }
 }
