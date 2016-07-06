5c6f326df1055c5c97851945112babaea6312792
7ASecond-Net <admin@7asecond.net>
2016-07-06 18:08:49 +0100

    Add ScintillaNET Test Form

---
+++ IDA/Forms/Dockable/FrmScintillaTest.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace IDA.Forms.Dockable
+{
+    partial class FrmScintillaTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.scintilla1 = new ScintillaNET.Scintilla();
+            this.SuspendLayout();
+            // 
+            // scintilla1
+            // 
+            this.scintilla1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.scintilla1.CaretStyle = ScintillaNET.CaretStyle.Block;
+            this.scintilla1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.scintilla1.IdleStyling = ScintillaNET.IdleStyling.All;
+            this.scintilla1.IndentationGuides = ScintillaNET.IndentView.Real;
+            this.scintilla1.Lexer = ScintillaNET.Lexer.Cpp;
+            this.scintilla1.Location = new System.Drawing.Point(0, 0);
+            this.scintilla1.Name = "scintilla1";
+            this.scintilla1.Size = new System.Drawing.Size(904, 614);
+            this.scintilla1.TabIndex = 0;
+            this.scintilla1.Text = "scintilla1";
+            this.scintilla1.WrapIndentMode = ScintillaNET.WrapIndentMode.Indent;
+            this.scintilla1.WrapMode = ScintillaNET.WrapMode.Whitespace;
+            this.scintilla1.WrapVisualFlags = ((ScintillaNET.WrapVisualFlags)((ScintillaNET.WrapVisualFlags.End | ScintillaNET.WrapVisualFlags.Start)));
+            // 
+            // FrmScintillaTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(904, 614);
+            this.Controls.Add(this.scintilla1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "FrmScintillaTest";
+            this.Text = "FrmScintillaTest";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ScintillaNET.Scintilla scintilla1;
+    }
+}
