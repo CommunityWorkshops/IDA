2ffc8695e39377e46b230c48aadcaa6e7b45a12d
7ASecond-Net <admin@7asecond.net>
2016-07-16 01:30:45 +0100

    Added Com Port Selector Form

---
+++ IDA/Forms/Dialog/FrmComPortSelector.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace IDA.Forms.Dialog
+{
+    partial class FrmComPortSelector
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmComPortSelector));
+            this.lbComPorts = new System.Windows.Forms.ListBox();
+            this.btnUse = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbComPorts
+            // 
+            this.lbComPorts.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbComPorts.FormattingEnabled = true;
+            this.lbComPorts.Location = new System.Drawing.Point(13, 13);
+            this.lbComPorts.Name = "lbComPorts";
+            this.lbComPorts.Size = new System.Drawing.Size(259, 199);
+            this.lbComPorts.TabIndex = 0;
+            // 
+            // btnUse
+            // 
+            this.btnUse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnUse.Location = new System.Drawing.Point(197, 226);
+            this.btnUse.Name = "btnUse";
+            this.btnUse.Size = new System.Drawing.Size(75, 23);
+            this.btnUse.TabIndex = 1;
+            this.btnUse.Text = "Use";
+            this.btnUse.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.Location = new System.Drawing.Point(107, 226);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // FrmComPortSelector
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnUse);
+            this.Controls.Add(this.lbComPorts);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmComPortSelector";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Com Port Selector";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbComPorts;
+        private System.Windows.Forms.Button btnUse;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ IDA/Forms/Dialog/FrmComPortSelector.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IDA.Forms.Dialog
+{
+    public partial class FrmComPortSelector : Form
+    {
+        public FrmComPortSelector()
+        {
+            InitializeComponent();
+            string[] ports = SerialPort.GetPortNames();
+            
+
+            if (ports.Length == 0)
+            {
+                MessageBox.Show("No ports are used. Is your device connected?");
+                Close();
+            }
+
+
+            foreach (string port in ports)
+            {
+                lbComPorts.Items.Add(port);
+            }
+        }
+    }
+}
