890f752f0cb047db3150c89abc2704be9030b1c8
7ASecond-Net <admin@7asecond.net>
2016-07-16 01:30:11 +0100

    Added USB Class

---
+++ IDA/Controllers/Hardware/USB.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDA.Controllers.Hardware
+{
+    internal class Usb
+    {
+
+      public  static List<UsbDeviceInfo> GetUsbDevices()
+        {
+            List<UsbDeviceInfo> devices = new List<UsbDeviceInfo>();
+
+            var searcher = new ManagementObjectSearcher(@"Select * From Win32_SerialPort");
+
+            foreach (var device in searcher.Get())
+            {
+                devices.Add(new UsbDeviceInfo(
+                (string)device.GetPropertyValue("DeviceId"),
+                (string)device.GetPropertyValue("PNPDeviceID"),
+                (string)device.GetPropertyValue("Description"),
+                (string)device.GetPropertyValue("Name")
+                ));
+            }
+
+            return devices;
+        }
+
+        internal class UsbDeviceInfo
+        {
+            public UsbDeviceInfo(string deviceId, string pnpDeviceId, string description, string name)
+            {
+                this.DeviceId = deviceId;
+                this.PnpDeviceId = pnpDeviceId;
+                this.Description = description;
+                this.Name = name;
+            }
+            public string DeviceId { get; private set; }
+            public string PnpDeviceId { get; private set; }
+            public string Description { get; private set; }
+            public string Name { get; private set; }
+
+        }
+
+    }
+}
