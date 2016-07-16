d00b3f19f8bcdd8b6937ea04666b565a93a4fbe6
7ASecond-Net <admin@7asecond.net>
2016-07-16 02:33:44 +0100

    Completed

--- IDA/Forms/Dialog/FrmComPortSelector.cs
+++ IDA/Forms/Dialog/FrmComPortSelector.cs
@@ -9,14 +9,21 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using IDA.Controllers.Hardware;
+using IDA.Models;
 
 namespace IDA.Forms.Dialog
 {
     public partial class FrmComPortSelector : Form
     {
+
+        private string DeviceId = null;
+
         public FrmComPortSelector()
         {
             InitializeComponent();
+
+            dgvComPorts.SelectionChanged += DgvComPorts_SelectionChanged;
+
             List<Usb.UsbDeviceInfo> ports = Usb.GetUsbDevices().ToList();
 
 
@@ -29,21 +36,28 @@ namespace IDA.Forms.Dialog
 
             foreach (Usb.UsbDeviceInfo port in ports)
             {
-
                 DataGridViewRow row = (DataGridViewRow)dgvComPorts.Rows[0].Clone();
                 row.Cells[0].Value = port.DeviceId;
+                if (DeviceId == null) DeviceId = port.DeviceId;
                 row.Cells[1].Value = port.Description;
-                dgvComPorts.Rows.Add(row);                   
+                dgvComPorts.Rows.Add(row);
             }
+        }
+
+        private void DgvComPorts_SelectionChanged(object sender, EventArgs e)
+        {
+            DeviceId = dgvComPorts.SelectedRows[0].Cells[0].Value.ToString();
         }
 
         private void btnUse_Click(object sender, EventArgs e)
         {
-
+            CurrentProjectModel.ComPort = DeviceId;
+            Close();
         }
 
         private void btnCancel_Click(object sender, EventArgs e)
         {
+            CurrentProjectModel.ComPort = null;
             Close();
         }
     }
