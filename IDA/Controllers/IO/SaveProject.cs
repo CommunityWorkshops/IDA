aea6065a71d5783a061ae8eafb1e666633d20f06
Dave Gordon <admin@7asecond.net>
2016-07-29 04:32:29 +0100

    Save Working

--- IDA/Controllers/IO/SaveProject.cs
+++ IDA/Controllers/IO/SaveProject.cs
@@ -1,18 +1,21 @@
-﻿using System;
+﻿using ScintillaNET;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
 
 namespace IDA.Controllers.IO
 {
     static class SaveProject
     {
 
-        internal static void Save()
+        // Tell this instance of the Editor to save it's contents 
+        internal static void Save(Control control)
         {
-
+            
         }
-
     }
 }
