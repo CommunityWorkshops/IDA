256573f7019d08d18f77ea455eb64c629b13187c
Dave Gordon <admin@7asecond.net>
2016-08-05 10:23:56 +0100

    Added Component Template Loader

---
+++ IDA/Controllers/IO/ComponentTemplateLoader.cs
@@ -0,0 +1,135 @@
+﻿using IDA.Models;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDA.Controllers.IO
+{
+    class ComponentTemplateLoader
+    {
+
+        public delegate void LogHandler(string message);
+        public event LogHandler ComponentTemplateLoaderLog;
+
+        public ComponentModel LoadComponents()
+        {
+            ComponentModel cm = new Models.ComponentModel();
+            var platformPath = @"templates\platforms\" + CurrentProjectModel.Platform + @"\components\";
+            FileStream fs = null;
+            StreamReader sr = null;
+
+            try
+            {
+                var componentBasePath = Path.Combine(platformPath, "InternalComponents.txt");
+                fs = new FileStream(componentBasePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+                sr = new StreamReader(fs);
+
+                while(!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine();
+                    if(!line.StartsWith(@"//") && line.Contains("="))
+                    {
+                        string[] lineParts = line.Split('=');
+                        switch (lineParts[0].ToLower())
+                        {
+                            case "version":
+                                if(lineParts[1].ToLower() == CurrentProjectModel.Version.ToLower())
+                                {
+                                    var versionLine = sr.ReadLine();
+                                    string[] versionParts = versionLine.Split(':');
+                                    switch (versionParts[0].ToLower())
+                                    {
+                                        case "name":
+                                            cm.componentName = versionParts[1].ToString();
+                                            break;
+                                        case "description":
+                                            cm.componentDescription = versionParts[1].ToString();
+                                            break;
+                                        case "options":
+                                            string[] options = versionParts[1].Split(',');
+                                            foreach(string option in options)
+                                            {
+                                                if (option.ToLower() == "analog")
+                                                    cm.componentOptions.Add(ComponentModel.Options.Analog);
+                                                if (option.ToLower() == "digital")
+                                                    cm.componentOptions.Add(ComponentModel.Options.Digital);
+                                            }
+                                            break;
+                                        case "icon":
+                                            // generate full path to the icon
+                                            var iconPath = Path.Combine(componentBasePath, @"images\internal\icons\" + versionParts[1]);
+                                            cm.componentIcon = Image.FromFile(iconPath);
+                                            break;
+                                        case "images":
+                                            var imagePath = Path.Combine(componentBasePath, @"images\internal\" + versionParts[1]);
+                                            cm.componentSchematic = Image.FromFile(Path.Combine(imagePath, "schematic.png"));
+                                            cm.componentLayout = Image.FromFile(Path.Combine(imagePath, "layout.png"));
+                                            break;
+                                        case "global":
+                                            cm.componentGlobal.Add(versionParts[1]);
+                                            break;
+                                        case "analog global":
+                                            cm.componentAnalogGlobal.Add(versionParts[1]);
+                                            break;
+                                        case "digital global":
+                                            cm.componentDigitalGlobal.Add(versionParts[1]);
+                                            break;
+                                        case "setup":
+                                            cm.componentSetup.Add(versionParts[1]);
+                                            break;
+                                        case "analog setup":
+                                            cm.componentAnalogSetup.Add(versionParts[1]);
+                                            break;
+                                        case "digital setup":
+                                            cm.componentDigitalSetup.Add(versionParts[1]);
+                                            break;
+                                        case "loop":
+                                            cm.componentLoop.Add(versionParts[1]);
+                                            break;
+                                        case "analog loop":
+                                            cm.componentAnalogLoop.Add(versionParts[1]);
+                                            break;
+                                        case "digital loop":
+                                            cm.componentDigitalLoop.Add(versionParts[1]);
+                                            break;
+                                        default:
+                                            //Ignore everything else for now
+                                            break;
+                                    }
+                                }
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+
+                sr?.Close();
+                fs?.Close();
+            }
+            catch (Exception ex)
+            {
+                OnLog(ex.Message);
+                sr?.Close();
+                fs?.Close();
+            }
+            finally
+            {
+                sr?.Close();
+                fs?.Close();
+            }
+
+
+            return cm;
+        }
+
+        private void OnLog(string message)
+        {
+            ComponentTemplateLoaderLog?.Invoke(message);
+        }
+    }
+}
---
+++ IDA/Models/ComponentModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDA.Models
+{
+    [Serializable]
+    class ComponentModel
+    {
+        public enum Options
+        {
+            Analog,
+            Digital
+        };
+
+        public string componentName { get; set; }
+        public string componentDescription { get; set; }
+        public List<Options> componentOptions { get; set; }
+        public Image componentSchematic { get; set; }
+        public Image componentLayout { get; set; }
+        public List<string> componentGlobal { get; set; }
+        public List<string> componentAnalogGlobal { get; set; }
+        public List<string> componentDigitalGlobal { get; set; }
+        public List<string> componentSetup { get; set; }
+        public List<string> componentAnalogSetup { get; set; }
+        public List<string> componentDigitalSetup { get; set; }
+        public List<string> componentLoop { get; set; }
+        public List<string> componentAnalogLoop { get; set; }
+        public List<string> componentDigitalLoop { get; set; }
+
+        //version 1.03
+        public Image componentIcon { get; set; }
+
+
+    }
+}
+
+// Version History
+// Template Format 1.03
+// Added Icon
+// Icon = The path to the image which is displayed in the toolbox for this component
+
+
+// Template format 1.02
+// Version = comma list of platform versions that this template belongs to.
+// Name = Displayed in the Toolbox
+// Description = Displayed at the bottom of the toolbox
+// Options = Comma list of which type of IO is available currently only Analog and Digital
+// Images = the Root folder to Schematic and Layout images in subfolder of Analog or Digital
+// Global = Global settings used in all examples
+// Analog Global = Global settings used in Analog Only Examples
+// Digital Global = Global settings used in Digital only examples
+// Setup = Code used in the Setup function required by all examples
+// Analog Setup = Code used in the Setup function required by analog examples
+// Digital Setup = Code used in the Setup function required by digital examples
+// Loop = Code used in the Loop function required by all examples
+// Analog Loop = Code used in the Loop function required by analog examples
+// Digital Loop = Code used in the Loop function required by digital examples
+//
+// Notes: 
+// The template parser: 
+//    ignores all lines starting with //
+//    splits parsed lines on the = sign
+//    Blank lines have no content after the = sign                    
+//    Ignores case in the tag prior to the = sign
+//    the content after the = sign is case sensitive
+//    Parser stops reading the document when it finds a line starting with END in capitals
+//    Parser starts reading the document when it finds a line starting with START in capitals
